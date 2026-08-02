1ffca7745c5b602755c9b9e315cc47c916b3bea5
S29Duckys <naelbarasraso@gmail.com>
2026-08-02 23:29:53 +0000

    feat(app): add the input and the select for select urgence value

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-function App() {
+type Priority = "Urgente" | "Moyenne" | "Basse";
 
+type Todo = {
+  id: number;
+
+  text: string;
+
+  priority: Priority;
+};
+
+function App() {
   return (
-    <div>
-      <button className="btn btn-accent">send</button>
+    <div className="flex justify-center">
+      <div className="w-2/3 flex flex-col gap-4 my-15 bg-base-300 p-5 rounded-2xl">
+        <div className="flex gap-4">
+          <input type="text" className="input w-full" placeholder="add task" />
+
+          <select className="w-full">
+            <option value="Urgent">Urgent</option>
+
+            <option value="Moyenne">Moyenne</option>
+
+            <option value="Bass">Bass</option>
+          </select>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
