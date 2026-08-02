e20c98b2a8dcb2e2a6ce3addc6981d1d09bba98b
S29Duckys <naelbarasraso@gmail.com>
2026-08-02 23:33:33 +0000

    feat(app): the form ui for add the tack is finish

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
     <div className="flex justify-center">
       <div className="w-2/3 flex flex-col gap-4 my-15 bg-base-300 p-5 rounded-2xl">
         <div className="flex gap-4">
-          <input type="text" className="input w-full" placeholder="add task" />
+          <input type="text" className="input w-full" placeholder="task..." />
 
           <select className="w-full">
             <option value="Urgent">Urgent</option>
@@ -22,6 +22,7 @@ function App() {
 
             <option value="Bass">Bass</option>
           </select>
+          <button className="btn btn-primary">add</button>
         </div>
       </div>
     </div>
