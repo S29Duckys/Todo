7c1ae760758e4098f445786b13a363ee5a4806c2
S29Duckys <naelbarasraso@gmail.com>
2026-08-03 10:21:13 +0000

    feat(form): from not finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { LucideReceiptIndianRupee } from "lucide-react";
+import { useState } from "react";
+
 type Priority = "Urgente" | "Moyenne" | "Basse";
 
 type Todo = {
@@ -8,21 +11,43 @@ type Todo = {
   priority: Priority;
 };
 
+
 function App() {
+  const [input, setInput] = useState<string>("")
+  const [priority, setPriority] = useState<Priority>("Moyenne")
+  const [todos, settodos] = useState<Todo[]>([])
+
+  function addTodo(){
+    if (input.trim() == ""){
+      return 
+    }
+
+    const newTodo : Todo = {
+      id: Date.now(),
+      text: input.trim(),
+      priority: priority,
+    }
+
+    const newTodos = [newTodo, ...todos]
+    settodos(newTodos)
+    setInput("")
+    setPriority("Moyenne")
+  }
+
   return (
     <div className="flex justify-center">
       <div className="w-2/3 flex flex-col gap-4 my-15 bg-base-300 p-5 rounded-2xl">
         <div className="flex gap-4">
-          <input type="text" className="input w-full" placeholder="task..." />
+          <input type="text" className="input w-full" placeholder="task..." value={input} onChange={(e) => setInput(e.target.value)}/>
 
-          <select className="w-full">
+          <select className="w-full" value={priority} onChange={(e) => setPriority(e.target.value as Priority)}>
             <option value="Urgent">Urgent</option>
 
             <option value="Moyenne">Moyenne</option>
 
             <option value="Bass">Bass</option>
           </select>
-          <button className="btn btn-primary">add</button>
+          <button className="btn btn-primary" onClick={addTodo}>add</button>
         </div>
       </div>
     </div>
