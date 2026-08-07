14fb1d8030bda596c06ce3a3eb961b0460da1aea
S29Duckys <naelbarasraso@gmail.com>
2026-08-07 16:23:06 +0000

    feat(todos): add TodoItem component with delete, priority counters and empty state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import TodoItem from "./Todoitem";
+import { Construction } from "lucide-react";
 
 type Priority = "Urgente" | "Moyenne" | "Basse";
 
@@ -15,7 +17,7 @@ function App() {
   const [priority, setPriority] = useState<Priority>("Moyenne");
   const savedTodo = localStorage.getItem("todos");
   const initialTodo = savedTodo ? JSON.parse(savedTodo) : [];
-  const [todos, settodos] = useState<Todo[]>([initialTodo]);
+  const [todos, settodos] = useState<Todo[]>(initialTodo);
   const [filter, setfilter] = useState<Priority | "All">("All")
 
   useEffect(() => {
@@ -49,6 +51,17 @@ function App() {
     filterTodos = todos.filter((todo) => todo.priority === filter)
   }
 
+  const urgentCount = todos.filter((t) => t.priority === "Urgente").length
+  const mediumCount = todos.filter((t) => t.priority === "Moyenne").length
+  const lowCount = todos.filter((t) => t.priority === "Basse").length
+
+  const totalCount = todos.length
+
+  function deletTodo(id: number){
+    const newTodo = todos.filter((todo) => todo.id !== id)
+    settodos(newTodo)
+  }
+
   return (
     <div className="flex justify-center">
       <div className="w-2/3 flex flex-col gap-4 my-15 bg-base-300 p-5 rounded-2xl">
@@ -66,11 +79,11 @@ function App() {
             value={priority}
             onChange={(e) => setPriority(e.target.value as Priority)}
           >
-            <option value="Urgent">Urgent</option>
+            <option value="Urgente">Urgente</option>
 
             <option value="Moyenne">Moyenne</option>
 
-            <option value="Bass">Bass</option>
+            <option value="Basse">Basse</option>
           </select>
           <button className="btn btn-primary" onClick={addTodo}>
             add
@@ -78,12 +91,26 @@ function App() {
         </div>
         <div className="space-y-2 flex-1 h-fit">
           <div className="flex flex-wrap gap-4">
-            <button className={`btn btn-soft ${filter === "All" ? "btm-primary" : ""}`} onClick={() => setfilter("All")}>All</button>
+            <button className={`btn btn-soft ${filter === "All" ? "btm-primary" : ""}`} onClick={() => setfilter("All")}>All ({totalCount})</button>
+            <button className={`btn btn-soft ${filter === "Urgente" ? "btm-primary" : ""}`} onClick={() => setfilter("Urgente")}>Urgent ({urgentCount})</button>
+            <button className={`btn btn-soft ${filter === "Moyenne" ? "btm-primary" : ""}`} onClick={() => setfilter("Moyenne")}>Medium ({mediumCount})</button>
+            <button className={`btn btn-soft ${filter === "Basse" ? "btm-primary" : ""}`} onClick={() => setfilter("Basse")}>Low ({lowCount})</button>
           </div>
           {filterTodos.length > 0 ? (
-            <div>test1</div>
+            <ul className="divide-y divide-primary/20">
+              {filterTodos.map((todo) => (
+                <li key={todo.id}>
+                  <TodoItem todo={todo} onDelete={() => deletTodo(todo.id)}/>
+                </li>
+              ))}
+            </ul>
           ) : (
-            <div>test2</div>
+            <div className="flex justify-center items-center flex-col p-5">
+              <div>
+                <Construction strokeWidth={1} className="w-40 h-40 test-primary"/>
+                <p className="texte-sm">Aucune tache pour ce filtre</p>
+              </div>
+            </div>
           )}
         </div>
       </div>
